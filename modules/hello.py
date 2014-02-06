04e87a4f232d764ce439c775968112eaa197d8fa
Ted <ted@stuckinacan.com>
2014-02-05 23:17:57 -0800

    adding documentation to sample plugin

--- modules/hello.py
+++ modules/hello.py
@@ -1,3 +1,9 @@
+"""A simple hello command for the masses
+Plugins take 4 arguments:
+	:bot an instance of URLBot
+	:channel the channel from which the message that invoked the command originated,
+	:msg the actual message containing the command and any arguments
+	:user the nickname of the user from which the commmand was issued
+"""
 def main(bot, channel, msg, user):
-	bot.msg(channel.encode('UTF-8'), 'msg: %s' % msg)
 	bot.msg(channel.encode('UTF-8'), 'Hello %s' % user)
